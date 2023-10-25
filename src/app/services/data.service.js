f0068d2672e6739eb60deacb444ed4875d89734f
Alexandre Fornari <alexandrefornari@gmail.com>
2023-10-25 17:45:29 -0300

    v2

--- src/app/services/data.service.js
+++ src/app/services/data.service.js
@@ -23,7 +23,7 @@
       data.success_score = 0.7;
 
       data.data = {
-        version: "0.18",
+        version: "0.19",
         currentTry:0,
         currentModule:0,
         currentScreen:0,
